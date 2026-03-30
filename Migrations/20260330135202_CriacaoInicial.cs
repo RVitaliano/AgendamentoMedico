e8e0ba20a2079e9e5d101c1280274c3dfbb45e4b
RVitaliano <bebetto1610@gmail.com>
2026-03-30 10:59:05 -0300

    feat: adicionando migrations e config database

---
+++ Migrations/20260330135202_CriacaoInicial.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgendamentoMedico.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    NomeCompleto = table.Column<string>(type: "longtext", nullable: false),
+                    CRM = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Especialidade = table.Column<string>(type: "longtext", nullable: false),
+                    Email = table.Column<string>(type: "longtext", nullable: false),
+                    Telefone = table.Column<string>(type: "longtext", nullable: false),
+                    Ativo = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    NomeCompleto = table.Column<string>(type: "longtext", nullable: false),
+                    CPF = table.Column<string>(type: "varchar(255)", nullable: false),
+                    DataNascimento = table.Column<DateOnly>(type: "date", nullable: false),
+                    Email = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Telefone = table.Column<string>(type: "longtext", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Agendamentos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    PacienteId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    MedicoId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    DataHora = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DuracaoMinutos = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "longtext", nullable: false),
+                    Observacoes = table.Column<string>(type: "longtext", nullable: true),
+                    DataCriacao = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agendamentos_Medicos_MedicoId",
+                        column: x => x.MedicoId,
+                        principalTable: "Medicos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Agendamentos_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendamentos_MedicoId",
+                table: "Agendamentos",
+                column: "MedicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendamentos_PacienteId",
+                table: "Agendamentos",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medicos_CRM",
+                table: "Medicos",
+                column: "CRM",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_CPF",
+                table: "Pacientes",
+                column: "CPF",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_Email",
+                table: "Pacientes",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agendamentos");
+
+            migrationBuilder.DropTable(
+                name: "Medicos");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,37 +1,28 @@
+using Microsoft.EntityFrameworkCore.Migrations;
 using AgendamentoMedico.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwaggerUI(options  =>
+    app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/openapi/v1.json", "Agendamento v1");
     });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
